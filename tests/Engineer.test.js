6c73936ba4a9e1bca459bfc8f4633c9c3556307d
Tyler Davis <tdnc85@yahoo.com>
2021-06-30 20:05:45 -0400

    added Engineer.test.js

---
+++ tests/Engineer.test.js
@@ -0,0 +1,22 @@
+const { test, expect } = require("@jest/globals");
+const Engineer = require("../library/Engineer");
+
+test("Can make Engineer odject", () => {
+    const e = new Engineer();
+    expect(typeof(e)).toBe("object")
+});
+test("set GitHub", () => {
+    const gitHub = "Twdavis85";
+    const e = new Engineer("foo", 10, "email", gitHub);
+    expect(e.gitHub).toBe(gitHub)
+});
+test("get role", () => {
+    const role = "Engineer";
+    const e = new Engineer("foo", 10, "email", "GitHub");
+    expect(e.getRole()).toBe(role)
+});
+test("get GitHub", () => {
+    const gitHub  = "Twdavis85";
+    const e = new Engineer("foo", 10, "email", gitHub);
+    expect(e.getGitHub()).toBe(gitHub)
+});
